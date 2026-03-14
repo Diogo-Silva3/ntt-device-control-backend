7c8acc7296715585f1009c1be513899347a45271
Diogo Silva <ofcdiogo.silva@gmail.com>
2026-03-14 13:56:02 -0300

    fix cors

--- src/server.js
+++ src/server.js
@@ -20,7 +20,12 @@ const PORT = process.env.PORT || 3001;
 
 // Middlewares
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://nttdevicecontrol.web.app',
+    'https://nttdevicecontrol.firebaseapp.com',
+    process.env.FRONTEND_URL,
+  ].filter(Boolean),
   credentials: true,
 }));
 app.use(express.json());
